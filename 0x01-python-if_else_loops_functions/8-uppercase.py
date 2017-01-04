b016c164b3610a3d258897f865937d9a84477919
Kina Lee <kina.lee@holbertonschool.com>
2017-01-04 07:23:05 +0000

    THANK YOU JAYgit add 8-uppercase.py!

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            upper = chr(ord(i) - 32)
+        else:
+            upper = i
+
+        print("{}".format(upper), end="")
+
+    print("")
