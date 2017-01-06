88aa5d62e80691e00eb3c4f7212c5c0285377e15
Kina Lee <kina.lee@holbertonschool.com>
2017-01-06 01:21:42 +0000

    if 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     if len(argv) == 0:
         print("{:d} argument.".format(len(argv) - 1))
     else:
+        if len(agv) == 1:
+            print("{:d} argument.".format(len(argv) - 1))
+
         print("{:d} arguments.".format(len(argv) - 1))
         for i in range(1, len(argv)):
             print("{:d}: {:s}".format(i, argv[i]))
