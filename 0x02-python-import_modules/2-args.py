cbb8ddc032eef40e6f50d2c509ccae40ac335a00
Kina Lee <kina.lee@holbertonschool.com>
2017-01-06 01:44:32 +0000

    -_-^

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,10 @@ from sys import argv
 if __name__ == "__main__":
     if len(argv) == 1 or len(argv) == 2:
         print("{:d} argument.".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{:d} argument:".format(len(argv) - 1))
     else:
-            print("{:d} arguments.".format(len(argv) - 1))
+            print("{:d} arguments:".format(len(argv) - 1))
 
     if len(argv) > 1:
         for i in range(1, len(argv)):
