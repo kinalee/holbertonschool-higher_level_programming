49fed1bcb1ff50440fc505339cb89ae87308b2a0
Kina Lee <kina.lee@holbertonschool.com>
2017-01-06 01:37:24 +0000

    hurl

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    if len(argv) == 0:
+    if len(argv) == 0 or len(argv) == 1:
         print("{:d} argument.".format(len(argv) - 1))
     else:
-        if len(agv) == 1:
-            print("{:d} argument.".format(len(argv) - 1))
+            print("{:d} arguments.".format(len(argv) - 1))
 
-        print("{:d} arguments.".format(len(argv) - 1))
+    if len(argv) > 1:
         for i in range(1, len(argv)):
             print("{:d}: {:s}".format(i, argv[i]))
