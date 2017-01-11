1867be76e200e20d04fdbbff95538ad3254b3415
Kina Lee <kina.lee@holbertonschool.com>
2017-01-11 01:46:09 +0000

    ????

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if (idx < 0 or idx >= len(list)):
-        return list
+    if (idx < 0 or idx >= len(my_list)):
+        return my_list
     else:
-        list[idx] = element
-        return list
+        my_list[idx] = element
+        return my_list
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,6 @@ def print_reversed_list_integer(my_list=[]):
     if my_list == []:
         print("")
     else:
-        list.reverse()
-        for i in list:
+        my_list.reverse()
+        for i in my_list:
             print("{:d}".format(i))
