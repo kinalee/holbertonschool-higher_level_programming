969e2b626908f131b1da666aa858873daba0b85f
Kina Lee <kina.lee@holbertonschool.com>
2017-01-11 01:19:35 +0000

    ...

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > len(my_list) or idx < 0 or list == []:
+    if idx > len(my_list) or idx < 0 or my_list == []:
         return None
     return list[idx]
