ab789e4831b71f59c9d53a4c5c290a585e4bf96d
Kina Lee <kina.lee@holbertonschool.com>
2017-01-11 04:34:35 +0000

    none?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_str = ""
+    my_list = []
     if my_string != []:
         for i in my_string:
             if (i != 'c' and i != 'C'):
-                new_str += i
-    return new_str
+                my_list.append(i)
+        return ("").join(my_list)
+    else:
+        return None
