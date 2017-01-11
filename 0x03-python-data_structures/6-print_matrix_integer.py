584db5f347d3c34afd97ba67dc4b7a57a3f4dc83
Kina Lee <kina.lee@holbertonschool.com>
2017-01-11 04:45:30 +0000

    yay

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
+        count = 0
         for j in matrix[i]:
-            print("{}".format(j), end=" ")
+            print("{:d}".format(j), end="")
+            count += 1
+            if count < len(matrix[i]):
+                print(" ", end="")
         print("")
