323b3f0b800dd2ee2ca609ed2393440604694189
Kina Lee <kina.lee@holbertonschool.com>
2017-01-13 02:14:17 +0000

    typo

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,8 @@
-2#!/usr/bin/python3
+#!/usr/bin/python3
 def update_dictionary(my_dict, key, value):
     for i in my_dict.keys():
         if i == key:
             my_dict[i] = value
-    if key not in my_dict: 
+    if key not in my_dict:
         my_dict[key] = value
     return my_dict
