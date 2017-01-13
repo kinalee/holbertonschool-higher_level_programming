d193fd74fd0d694259d882eac8135140c9eb81a5
Kina Lee <kina.lee@holbertonschool.com>
2017-01-13 03:34:48 +0000

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    tmp = 0
     if my_dict is None:
         return None
+    tmp = list(my_dict.values())
+    tmp = tmp[0]
     for i in my_dict:
         if my_dict[i] > tmp:
             tmp = my_dict[i]
