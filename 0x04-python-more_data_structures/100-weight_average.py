b64c5378b012675d94bd01c4e145689a0332b3d3
Kina Lee <kina.lee@holbertonschool.com>
2017-01-13 04:22:06 +0000

    100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    sum = 0
+    div = 0
+    if my_list == {}:
+        return 0
+    for i in my_list:
+        mul = 1
+        for j in i:
+            mul *= j
+        div += i[1]
+        sum += mul
+    return sum / div
