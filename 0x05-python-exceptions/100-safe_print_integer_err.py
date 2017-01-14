44ec916e2fde4483521294eec65aefaee881ba7d
Kina Lee <kina.lee@holbertonschool.com>
2017-01-14 07:32:17 +0000

    why?

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
     except Exception as err:
-        print("Exception:", err)
+        sys.stderr.write("Exception:: %s\n" % str(err))
         return False
