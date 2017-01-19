76574e16d4a40fe2143417759a7991b11cbd7c85
Kina Lee <kina.lee@holbertonschool.com>
2017-01-19 03:28:58 +0000

    4

---
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+4-print_square
+Prints a square with the character #
+Contains one module: print_square
+"""
+
+
+def print_square(size):
+    if isinstance(size, int) is not True:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) is True and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        for j in range(size):
+            print('#',end="")
+        print("")
