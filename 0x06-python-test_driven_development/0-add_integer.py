aff7430ebb5787472ab681751f0ce51bbbecbea1
Kina Lee <kina.lee@holbertonschool.com>
2017-01-19 01:06:12 +0000

    0

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -10,10 +10,8 @@ def add_integer(a, b):
     """
     Both a and b must be integers, a TypeError will be raise otherwise
     """
-    try:
-        return (int(a) + int(b))
-    except:
-        if isinstance(a, (int, float)) is not True:
-            raise TypeError("a must be an integer")
-        if isinstance(b, (int, float)) is not True:
-            raise TypeError("b must be an integer")
+    if isinstance(a, (int, float)) is not True:
+        raise TypeError("a must be an integer")
+    if isinstance(b, (int, float)) is not True:
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
