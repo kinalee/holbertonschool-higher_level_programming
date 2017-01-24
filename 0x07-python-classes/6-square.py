09740858d846a1c173ad47594eaa002aeaf8dc64
Kina Lee <kina.lee@holbertonschool.com>
2017-01-24 07:34:21 +0000

    아몰랑

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -46,11 +46,13 @@ class Square:
         - sets position
         - raise errors when value is not tuple of 2 positive integers
         """
-        if isinstance(value, tuple) is not True or len(value) != 2:
+        if isinstance(value, tuple) is not True:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if isinstance(value[0], value[1], int) is not True:
+        if isinstance(value[0], int) or isinstance(value[1], int) is not True:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
@@ -61,16 +63,17 @@ class Square:
 
     def my_print(self):
         """ prints in stdout the square with the character # """
+        p0 = self.position[0]
+        p1 = self.position[1]
         size = self.size
-        pos = self.position
         if size == 0:
             print("")
         else:
-            for p1 in range(pos[1]):
+            for p in range(p1):
                 print("")
             for i in range(size):
-                for p2 in range(pos[0]):
+                for j in range(p0):
                     print("", end=" ")
-                for j in range(size):
+                for k in range(size):
                     print("#", end="")
                 print("")
