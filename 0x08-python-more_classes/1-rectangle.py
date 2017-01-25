63186450cafdfe0caa25ac9040c3dce3441e4c44
Kina Lee <kina.lee@holbertonschool.com>
2017-01-25 05:55:06 +0000

    single quotes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,9 +24,9 @@ class Rectangle:
         - raises errors when value is not int or value is less than 0
         """
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError('width must be an integer')
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
@@ -41,7 +41,7 @@ class Rectangle:
         - raises errors when value is not int or value is less than 0
         """
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError('height must be an integer')
         elif value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError('height must be >= 0')
         self.__height = value
