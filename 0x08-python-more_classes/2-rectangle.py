cbaeebd36cbb48b64dc21fe2d29fc19b9ea98e26
Kina Lee <kina.lee@holbertonschool.com>
2017-01-25 06:00:47 +0000

    if 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,6 +6,7 @@ class that defines rectangle
 
 
 class Rectangle:
+    """ Rectangle class"""
     def __init__(self, width=0, height=0):
         """ initializes instance attribute width and height """
         self.width = width
@@ -49,4 +50,7 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+
         return (self.__width * 2) + (self.__height * 2)
