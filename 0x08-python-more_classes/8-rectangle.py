669a68296028b875f371ea5eaab3c52c7c293071
Kina Lee <kina.lee@holbertonschool.com>
2017-01-25 06:41:36 +0000

    rec

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -89,7 +89,7 @@ class Rectangle:
             raise TypeError('rect_1 must be an instance of Rectangle')
         if isinstance(rect_2, Rectangle) is not True:
             raise TypeError('rect_1 must be an instance of Rectangle')
-        if rect_1.area() > rect_2.area() or rect_1.area() == rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
         if rect_2.area() > rect_1.area():
             return rect_2
