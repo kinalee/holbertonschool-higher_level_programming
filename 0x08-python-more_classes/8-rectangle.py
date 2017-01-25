fbe067ecad193ef88b3aab5912bde5f0e8f765b3
Kina Lee <kina.lee@holbertonschool.com>
2017-01-25 06:31:35 +0000

    no. way.

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     """ Rectangle class """
 
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """ initializes instance attribute width and height """
