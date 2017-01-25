1d999691d75c9c29ed9b0f7c6ccb056343b40976
Kina Lee <kina.lee@holbertonschool.com>
2017-01-25 07:13:00 +0000

    order

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -53,6 +53,18 @@ class Rectangle:
         else:
             self.__height = value
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """ returns the bigger rectangle """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        if rect_2.area() > rect_1.area():
+            return rect_2
+
     def area(self):
         """ returns the area of rectangle """
         return self.__width * self.__height
@@ -82,15 +94,3 @@ class Rectangle:
         """ destructs method """
         Rectangle.number_of_instances -= 1
         print('Bye rectangle...')
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """ returns the bigger rectangle """
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError('rect_1 must be an instance of Rectangle')
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError('rect_1 must be an instance of Rectangle')
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        if rect_2.area() > rect_1.area():
-            return rect_2
