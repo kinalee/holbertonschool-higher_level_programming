fd573350ec787fbf35d61cc253d32d8a76e2be55
Kina Lee <kina.lee@holbertonschool.com>
2017-01-25 05:58:14 +0000

    single quotes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -6,8 +6,8 @@ class that defines rectangle
 
 
 class Rectangle:
-    """ initializes instance attribute width and height """
     def __init__(self, width=0, height=0):
+    """ initializes instance attribute width and height """
         self.width = width
         self.height = height
 
@@ -23,9 +23,9 @@ class Rectangle:
         - raise errors when value is not int or value is less than 0
         """
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError('width must be an integer')
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
@@ -40,9 +40,9 @@ class Rectangle:
         - raise errors when value is not int or value is less than 0
         """
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError('height must be an integer')
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError('height must be >= 0')
         self.__height = value
 
     def area(self):
