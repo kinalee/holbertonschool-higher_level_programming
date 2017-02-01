9c45a24a84b80332782ea57873c0c048d8a84daa
Kina Lee <kina.lee@holbertonschool.com>
2017-02-01 22:15:15 +0000

    ..

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """
 1-my_list
-contains MyList clas
+contains MyList class
 """
 
 
 class MyList(list):
-    """
-    class MyList that inherits from list
-    """
+    """ class MyList that inherits from list """
     def print_sorted(self):
-        """
-        prints sorted list
-        """
+        """ prints sorted list """
         print(sorted(self))
