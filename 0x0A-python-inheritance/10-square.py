6fb4577d3b56ed73237fb38b213e32f04dc7fa2b
Kina Lee <kina.lee@holbertonschool.com>
2017-02-02 07:26:49 +0000

    oops

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,25 +6,6 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle(BaseGeometry):
-    """ Rectangle class """
-    def __init__(self, width, height):
-        """ initializes instance attribute width and height """
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """ returns the area of current rectangle """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ makes object readable """
-        return "[{:s}] {:d}/{:d}".format(__class__.__name__,
-                                         self.__width, self.__height)
-
-
 class Square(Rectangle):
     """ Square class """
     def __init__(self, size):
