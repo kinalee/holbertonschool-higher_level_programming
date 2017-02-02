a6c78019bde6ec178dd4aeaee3275b10436a5cc3
Kina Lee <kina.lee@holbertonschool.com>
2017-02-02 07:36:36 +0000

    size

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,44 +2,15 @@
 """
 11-square
 """
-
-
-class BaseGeometry:
-    """ BaseGeometry class"""
-    def area(self):
-        """ raises an Exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ validates value """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """ Rectangle class """
-    def __init__(self, width, height):
-        """ initializes instance attribute width and height """
-        self.__width = width
-        self.__height = height
-        BaseGeometry.integer_validator(self, self.__width, self.__height)
-
-    def area(self):
-        """ returns the area of current rectangle """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ makes object readable """
-        return "[{}] {:d}/{:d}".format(__class__.__name__,
-                                       self.__width, self.__height)
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """ Square class """
     def __init__(self, size):
         """ initializes instance attribute size"""
+        BaseGeometry.integer_validator(self, "size", size)
         super().__init__(size, size)
         self.__size = size
 
