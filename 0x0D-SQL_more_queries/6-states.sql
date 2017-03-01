dc0d8dbd4d5a22e9d308a08e0a6051e372f45ad1
Kina Lee <kina.lee@holbertonschool.com>
2017-03-01 21:00:54 +0000

    using use

--- 0x0D-SQL_more_queries/6-states.sql
+++ 0x0D-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- Creates the database hbtn_0d_usa and the table states in hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
 id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY,
 name VARCHAR(256) NOT NULL
 )ENGINE=INNODB;
