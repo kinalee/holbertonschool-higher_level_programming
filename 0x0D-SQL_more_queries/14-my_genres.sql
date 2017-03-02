798e9acdb5c365d948acd74d72fc31596fdb3a99
Kina Lee <kina.lee@holbertonschool.com>
2017-03-02 03:54:54 +0000

    removed spaces

--- 0x0D-SQL_more_queries/14-my_genres.sql
+++ 0x0D-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
 -- Lists all genres of the show Dexter
 SELECT tv_genres.name FROM tv_genres
-JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_show_genres.show_id = 8
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_show_genres.show_id=8
 ORDER BY tv_genres.name ASC;
