fae5de0af23a40d2293a03349e68ab70b9b7ef92
Kina Lee <kina.lee@holbertonschool.com>
2017-03-02 02:02:31 +0000

    id name

--- 0x0D-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0D-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id IN 
       (SELECT id
       FROM states
