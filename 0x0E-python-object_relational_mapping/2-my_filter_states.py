2c39d79f594ed10dc554e5ae0ebbff104e10650e
Kina Lee <kina.lee@holbertonschool.com>
2017-03-09 00:59:06 +0000

    let's see

--- 0x0E-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0E-python-object_relational_mapping/2-my_filter_states.py
@@ -3,18 +3,18 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user="{:s}".format(sys.argv[1]),
-                     passwd="{:s}".format(sys.argv[2]),
-                     db="{:s}".format(sys.argv[3]))
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user="{:s}".format(sys.argv[1]),
+                         passwd="{:s}".format(sys.argv[2]),
+                         db="{:s}".format(sys.argv[3]))
 
-cursor = db.cursor()
-cursor.execute(""" SELECT * FROM states WHERE name=%s
-ORDER BY states.id ASC """, (sys.argv[4],))
+    cursor = db.cursor()
+    cursor.execute(""" SELECT * FROM states WHERE name=%s
+    ORDER BY states.id ASC """, (sys.argv[4],))
 
-for data in cursor.fetchall():
-    print(data)
+    for data in cursor.fetchall():
+        print(data)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
--- 0x0E-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0E-python-object_relational_mapping/4-cities_by_state.py
@@ -3,18 +3,18 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
-cursor = db.cursor()
-cursor.execute(""" SELECT cities.id, cities.name, states.name FROM cities
-LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC """)
+    cursor = db.cursor()
+    cursor.execute(""" SELECT cities.id, cities.name, states.name FROM cities
+    LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC """)
 
-for data in cursor.fetchall():
-    print(data)
+    for data in cursor.fetchall():
+        print(data)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
