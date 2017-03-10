d8a2df07aa71e065c5581da2517fd881afc7983e
Kina Lee <kina.lee@holbertonschool.com>
2017-03-10 03:06:42 +0000

    typos fixed

--- 0x0E-python-object_relational_mapping/model_state.py
+++ 0x0E-python-object_relational_mapping/model_state.py
@@ -5,11 +5,11 @@ from sqlalchemy import Column, Integer, String
 First state model
 """
 
-Base = declarative_base():
+Base = declarative_base()
 
 class State(Base):
     __tablename__ = 'states'
 
     id = Column(Integer, autoincrement=True, nullable=False,
                 primary_key=True, unique=True)
-    name = Column(String(128), nullabe=False)
+    name = Column(String(128), nullable=False)
