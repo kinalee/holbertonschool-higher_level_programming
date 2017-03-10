cced30be99de7ea5f423e0b13063d66e797547d4
Kina Lee <kina.lee@holbertonschool.com>
2017-03-10 04:55:16 +0000

    10

---
+++ 0x0E-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    state = sys.argv[4].split(";")
+    state = state[0]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for data in session.query(State).all():
+        if data.name == state:
+            print(data.id)
+            exit()
+    print("Not Found")
