458abd9e74d2b594cf2d7610bf11aee3985c2969
Kina Lee <kina.lee@holbertonschool.com>
2017-04-11 04:34:43 +0000

    spaces

--- 0x0F-python-network_0/39-methods.sh
+++ 0x0F-python-network_0/39-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -ILs "$1" | grep Allow | cut -d':' -f2 | tr -d ' '
+curl -ILs "$1" | grep Allow | cut -d':' -f2 | cut -c 2-
