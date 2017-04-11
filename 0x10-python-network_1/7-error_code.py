f36b5ba14146f7157d3e7035c170a5010918c438
Kina Lee <kina.lee@holbertonschool.com>
2017-04-11 23:13:11 +0000

    5, 6, 7

---
+++ 0x10-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL and
+displays the body of the response
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    try:
+        print(r.text)
+    except:
+        err = r.raise_for_status(status_code)
+        print("Error code: {:d}".format(err))
