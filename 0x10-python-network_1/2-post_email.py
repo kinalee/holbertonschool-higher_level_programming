1a1ba04c03584605cae8d6f0ff76b4345abe4942
Kina Lee <kina.lee@holbertonschool.com>
2017-04-11 21:48:52 +0000

    pep8

--- 0x10-python-network_1/2-post_email.py
+++ 0x10-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ with the email as a parameter, and displays the body of the response
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    value = {'email' : email}
+    value = {'email': email}
 
     data = urllib.parse.urlencode(value)
     data = data.encode('utf-8')
