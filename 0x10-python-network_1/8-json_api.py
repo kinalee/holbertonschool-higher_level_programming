37332843333e1486f3434347bf0cdcd3976d3766
Kina Lee <kina.lee@holbertonschool.com>
2017-04-12 01:13:46 +0000

    8

---
+++ 0x10-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if sys.argv[1]:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+    data = {'q': letter}
+
+    r = requests.post(url, data=data)
+    if r.json():
+        print("[{}] {}".format(r['id'], r['name']))
+    else:
+        if not r.json():
+            print("No result")
+        else:
+            print("Not a valid JSON")
