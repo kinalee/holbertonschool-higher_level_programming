3c4fc9fa9270a86f71b88fb18e7ea2b8054a7ae6
Kina Lee <kina.lee@holbertonschool.com>
2017-04-12 07:03:10 +0000

    9

---
+++ 0x10-python-network_1/9-starwars.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Takes in a string and sends a search request to the Star Wars API
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    swapi = "https://swapi.co/api/people/?search=" + sys.argv[1]
+    req = requests.get(swapi)
+    r = dict(req.json())
+    print("Number of result: {}".format(r['count']))
+
+    for data in r['results']:
+        print(data['name'])
