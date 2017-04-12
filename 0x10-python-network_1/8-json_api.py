93a0d8fa57ef843fdfff069cace5e54a418f3b4e
Kina Lee <kina.lee@holbertonschool.com>
2017-04-12 08:54:25 +0000

    no result fixed

--- 0x10-python-network_1/8-json_api.py
+++ 0x10-python-network_1/8-json_api.py
@@ -9,17 +9,20 @@ import requests
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) < 2:
+    if len(sys.argv) == 1:
+        print("No result")
+    if len(sys.argv) > 1 and type(sys.argv[1]) is int:
+        print("No result")
+    elif len(sys.argv) < 2:
         letter = ""
     else:
         letter = sys.argv[1]
-    data = {'q': letter}
-
-    r = requests.post(url, data=data)
-    try:
-        print("[{}] {}".format(r.json()['id'], r.json()['name']))
-    except:
-        if not r.json():
-            print("No result")
-        else:
-            print("Not a valid JSON")
+        data = {'q': letter}
+        r = requests.post(url, data=data)
+        try:
+            print("[{}] {}".format(r.json()['id'], r.json()['name']))
+        except:
+            if r.json()['id'] is None:
+                print("No result")
+            else:
+                print("Not a valid JSON")
