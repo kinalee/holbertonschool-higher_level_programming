b74087dd04077af134e6761a944ffe4f8f2a0506
Kina Lee <kina.lee@holbertonschool.com>
2017-04-12 00:48:52 +0000

    damn typo

--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     stat = r.status_code
     if stat >= 400:
-        print("Error code: {}".format(e))
+        print("Error code: {}".format(stat))
     else:
         print(r.text)
