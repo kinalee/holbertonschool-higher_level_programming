e86ca764e19981b744c6cc4a0898ce769f7e974d
Kina Lee <kina.lee@holbertonschool.com>
2017-04-28 00:33:23 +0000

    fixed

--- 0x11-javascript-warm_up/11-second_biggest.js
+++ 0x11-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Searches the second biggest integer in the list of arguments
 
-if (process.argv.length < 3) {
+if (process.argv.length < 4) {
   console.log(0);
 } else {
   console.log(process.argv.slice(2, process.argv.length).sort()[process.argv.length - 4]);
