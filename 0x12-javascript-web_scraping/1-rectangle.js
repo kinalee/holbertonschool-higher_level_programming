cedb8ac0149866b29c45a6e1a02f39bbe68c7f74
Kina Lee <kina.lee@holbertonschool.com>
2017-04-30 02:57:20 +0000

    func name fixed

--- 0x12-javascript-web_scraping/1-rectangle.js
+++ 0x12-javascript-web_scraping/1-rectangle.js
@@ -2,7 +2,7 @@
 // class Rectangle that defines a rectangle
 // The constructor must take 2 arguments w and h
 
-exports.Rectangle = function (w, h) {
+exports.Rectangle = function Rectangle (w, h) {
   // Initialize the instance attribute width with the value of w
   // Initialize the instance attribute height with the value of h
   this.width = w;
