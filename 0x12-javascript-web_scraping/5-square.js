f9592a5a53a7a0be977e5c4c8eecec9a76c7d69a
Kina Lee <kina.lee@holbertonschool.com>
2017-04-30 04:48:24 +0000

    require inside the functino

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 // class Square that defines a sqaure and inherits from Rectangle of 4-rectangle
 
-const Rectangle = require('./4-rectangle').Rectangle;
-
 exports.Square = function Square (size) {
+  const Rectangle = require('./4-rectangle').Rectangle;
   Rectangle.call(this, size, size);
 };
