cbb223ab744b15e020a53365752d5491c92a793b
Kina Lee <kina.lee@holbertonschool.com>
2017-04-30 04:24:17 +0000

    using var instead of const

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // class Square that defines a sqaure and inherits from Rectangle of 4-rectangle.js
 
-const Rectangle = require('./4-rectangle').Rectangle;
+var Rectangle = require('./4-rectangle').Rectangle;
 
 exports.Square = function Square (size) {
   Rectangle.call(this, constructor);
