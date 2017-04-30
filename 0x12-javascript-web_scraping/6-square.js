d13357be2f6730a52b32e479ad2959b6f1a5915e
Kina Lee <kina.lee@holbertonschool.com>
2017-04-30 04:44:33 +0000

    fixed

--- 0x12-javascript-web_scraping/6-square.js
+++ 0x12-javascript-web_scraping/6-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // class Square that defines a square and inherits from Square of 5-square.js
 
+const Square0 = require('./5-square').Square;
+
 exports.Square = function Square (size) {
-  const Square0 = require('./5-square').Square;
-  Square0.call(this);
-  this.size = size;
+  Square0.call(this, size, size);
 
   // Instance method called charPrint(c) that prints the rectangle using the character c
   // If c is undefined, use the character X
