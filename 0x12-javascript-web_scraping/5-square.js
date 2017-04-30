cc04054b60a3a08bf8db5103a572efdaa4b45468
Kina Lee <kina.lee@holbertonschool.com>
2017-04-30 06:54:51 +0000

    prototype and constructor handled

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -5,6 +5,6 @@ const Rectangle = require('./4-rectangle').Rectangle;
 
 exports.Square = function Square (size) {
   Rectangle.call(this, size, size);
+  Square.prototype = Object.create(Rectangle.prototype);
+  Square.prototype.constructor = Square;
 };
-
-Square.prototype = Object.create(Rectangle.prototype);
