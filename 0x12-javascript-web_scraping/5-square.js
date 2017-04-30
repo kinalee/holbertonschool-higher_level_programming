2ccfbae96370162bdbd7d0c85c888b401f92dde8
Kina Lee <kina.lee@holbertonschool.com>
2017-04-30 07:45:14 +0000

    module.exports.rect

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -3,7 +3,6 @@
 
 const Rectangle = require('./4-rectangle').Rectangle;
 
-exports.Square = function Square (size) {
-  this.size = size;
+module.exports.Square = function Square (size) {
   Rectangle.call(this, size, size);
 };
