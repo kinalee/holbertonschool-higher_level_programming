96746ee6eee84b79ef98e933f27927e19773b76d
Kina Lee <kina.lee@holbertonschool.com>
2017-04-30 02:58:15 +0000

    function name added

--- 0x12-javascript-web_scraping/3-rectangle.js
+++ 0x12-javascript-web_scraping/3-rectangle.js
@@ -2,7 +2,7 @@
 // class Rectangle that defines a rectangle
 // The constructor must take 2 arguments w and h
 
-exports.Rectangle = function (w, h) {
+exports.Rectangle = function Rectangle (w, h) {
   // Initialize the instance attribute width with the value of w
   // Initialize the instance attribute height with the value of h
   // If w or h is equal to 0 or not a positive integer, create an empty object
