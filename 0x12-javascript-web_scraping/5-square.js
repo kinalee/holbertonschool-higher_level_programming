c6e3f7c6d9bc1f6b23ae92ae2ecb6465eb2a1a59
Kina Lee <kina.lee@holbertonschool.com>
2017-04-30 04:31:24 +0000

    not giving a value to this.size

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -5,6 +5,4 @@ const Rectangle = require('./4-rectangle').Rectangle;
 
 exports.Square = function Square (size) {
   Rectangle.prototype.constructor.call(this, size, size);
-  this.width = size;
-  this.height = size;
 };
