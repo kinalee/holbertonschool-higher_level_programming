c919e7c25054eedc744514021a58c6b478258519
Kina Lee <kina.lee@holbertonschool.com>
2017-05-01 08:20:53 +0000

    6

--- 0x12-javascript-web_scraping/6-square.js
+++ 0x12-javascript-web_scraping/6-square.js
@@ -6,16 +6,16 @@ const Square0 = require('./5-square').Square;
 exports.Square = function Square (size) {
   if (size > 0) {
     Square0.call(this, size);
-    this.size = size;
-  }
+
   // Instance method called charPrint(c) that prints the rectangle using the character c
   // If c is undefined, use the character X
-  Square.prototype.charPrint = function (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.size; ++i) {
-      console.log(c.repeat(this.size));
-    }
-  };
+    Square.prototype.charPrint = function (c) {
+      if (c === undefined) {
+        c = 'X';
+      }
+      for (let i = 0; i < this.width; ++i) {
+        console.log(c.repeat(this.height));
+      }
+    };
+  }
 };
