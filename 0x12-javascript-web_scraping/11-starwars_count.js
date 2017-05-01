c9ce65039f34888dbd7810f4ecf3a1d2cdd67ee2
Kina Lee <kina.lee@holbertonschool.com>
2017-05-01 06:37:14 +0000

    11 working

--- 0x12-javascript-web_scraping/11-starwars_count.js
+++ 0x12-javascript-web_scraping/11-starwars_count.js
@@ -1,10 +1,21 @@
 #!/usr/bin/node
 // prints the number of movies where the character "Wedge Antilles" is present
 
-const url = 'http://swapi.co/api/films/';
+const url = process.argv[2];
 const wedge = 'http://swapi.co/api/people/18/';
 const request = require('request');
-request(url, function (err, data) {
+request(url, function (err, body) {
   if (err) {
     console.error(err);
-  }
+  } else {
+      let count = 0;
+      for (let data of JSON.parse(body.body)['results']) {
+        for (let w of data['characters']) {
+          if (w == wedge) {
+            ++count;
+          }
+        }
+      }
+    console.log(count);
+    }
+});
