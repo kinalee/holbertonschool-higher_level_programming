f84251c55191229cdbba3bb24c385629f26d7d9f
Kina Lee <kina.lee@holbertonschool.com>
2017-05-03 23:37:11 +0000

    22, 23

--- 0x13-javascript-web_jquery/22-script.js
+++ 0x13-javascript-web_jquery/22-script.js
@@ -1,4 +1,5 @@
-$.get('http://swapi.co/api/films?format=json', function (body) {
+// fetches and lists all movies title by using Star Wars API
+$.getJSON('http://swapi.co/api/films?format=json', function (body) {
   $.each(body.results, function (key, value) {
     $('UL#list_movies').append($('<LI></LI>').text(value.title));
   });
