aa258f59acd0091568405c0a771c92e9c8997b6e
Kina Lee <kina.lee@holbertonschool.com>
2017-05-03 22:57:54 +0000

    one class at a time

--- 0x13-javascript-web_jquery/18-script.js
+++ 0x13-javascript-web_jquery/18-script.js
@@ -1,5 +1,5 @@
 // toggles the class of the HTML tag HEADER to red (#FF0000) when the user clicks on the tag DIV#toggle_header
 $('DIV#toggle_header').click(function () {
-  $('HEADER').addClass('red');
+  $('HEADER').toggleClass('red');
   $('HEADER').toggleClass('green');
 });
